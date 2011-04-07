79d78ae31b1351ce18a1b66df3707136a24cf57a
Sven Tschui <info@eniu.ch>
2011-04-07 21:28:29 +0200

    Added/Updated users.xhtml and profile.xhtml

--- src/beans/UserBean.java
+++ src/beans/UserBean.java
@@ -1,7 +1,11 @@
 package beans;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import models.UserModel;
 
@@ -12,5 +16,18 @@ public class UserBean {
 	
 	public void logout() {
 		
+	}
+	
+	public UserModel getUser() throws Throwable {
+		Map<String, String> request = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		Integer userId = Integer.parseInt(request.get("userId"));
+		
+		if(userId == null) throw new Exception("Invalid Request");
+		
+		return new UserModel(userId);
+	}
+	
+	public ArrayList<UserModel> getUsers() throws Throwable {
+		return UserModel.getUsers(0, 20);
 	}
 }
