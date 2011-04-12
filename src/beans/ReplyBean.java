f3e53955a0d1a84f48ad29842302ac7dad5a9e32
Sven Tschui <info@eniu.ch>
2011-04-12 14:19:25 +0200

    Added ReplyBean

---
+++ src/beans/ReplyBean.java
@@ -0,0 +1,33 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import models.ReplyModel;
+
+@ManagedBean
+@RequestScoped
+public class ReplyBean {
+	private String content;
+	
+	public String save(int threadId, int userId) throws Throwable {
+		System.out.println(userId);
+		ReplyModel reply = new ReplyModel();
+		reply.setContent(content);
+		reply.setThreadId(threadId);
+		reply.setUserId(userId);
+		reply.save();
+		
+		content = "";
+		
+		return null;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
