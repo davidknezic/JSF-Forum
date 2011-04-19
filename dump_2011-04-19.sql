cdd91d70f382f640714a04253b29b65b3d8e6172
David Knezic <davidknezic@gmail.com>
2011-04-19 14:13:02 +0200

    Added new sql dump

--- dump_2011-04-19.sql
+++ dump_2011-04-19.sql
@@ -2,9 +2,9 @@
 # Version 2492
 # http://code.google.com/p/sequel-pro
 #
-# Host: 127.0.0.1 (MySQL 5.5.9)
+# Host: 127.0.0.1 (MySQL 5.1.46)
 # Database: forum
-# Generation Time: 2011-04-11 19:29:47 +0200
+# Generation Time: 2011-04-19 14:10:48 +0200
 # ************************************************************
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -39,7 +39,7 @@ VALUES
 	(1,1,'General','General questions'),
 	(2,2,'(X)HTML','Structuring websites'),
 	(4,2,'CSS','Styling websites'),
-	(5,3,'PHP','Hypetrext Preprocessor'),
+	(5,3,'PHP','Hypertext Preprocessor'),
 	(6,3,'JSF','Java Server Faces');
 
 /*!40000 ALTER TABLE `board` ENABLE KEYS */;
@@ -85,13 +85,19 @@ CREATE TABLE `reply` (
   KEY `userId` (`userId`),
   CONSTRAINT `reply_ibfk_1` FOREIGN KEY (`threadId`) REFERENCES `thread` (`threadId`) ON DELETE CASCADE,
   CONSTRAINT `reply_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `reply` WRITE;
 /*!40000 ALTER TABLE `reply` DISABLE KEYS */;
 INSERT INTO `reply` (`replyId`,`threadId`,`userId`,`content`,`createdOn`)
 VALUES
-	(1,2,3,'I like the technology behind this forum, hehehehe :)','2011-04-05 23:09:14');
+	(1,2,3,'I like the technology behind this forum!','2011-04-04 23:09:14'),
+	(2,2,1,'Ich Holz','2011-04-11 14:20:44'),
+	(3,2,1,'trololol','2011-04-12 14:29:31'),
+	(4,2,1,'jojojo','2011-04-01 16:10:11'),
+	(5,4,1,'dsfsdfdsf','2011-04-15 21:43:13'),
+	(6,8,1,'Sali','2011-04-19 10:44:52'),
+	(7,8,1,'dwfwefewf','2011-04-19 10:53:34');
 
 /*!40000 ALTER TABLE `reply` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -108,20 +114,25 @@ CREATE TABLE `thread` (
   `userId` int(10) unsigned NOT NULL,
   `title` varchar(512) NOT NULL,
   `content` text NOT NULL,
-  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdOn` datetime NOT NULL,
   PRIMARY KEY (`threadId`),
   KEY `boardId` (`boardId`),
   KEY `userId` (`userId`),
   CONSTRAINT `thread_ibfk_1` FOREIGN KEY (`boardId`) REFERENCES `board` (`boardId`) ON DELETE CASCADE,
   CONSTRAINT `thread_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `thread` WRITE;
 /*!40000 ALTER TABLE `thread` DISABLE KEYS */;
 INSERT INTO `thread` (`threadId`,`boardId`,`userId`,`title`,`content`,`createdOn`)
 VALUES
 	(2,1,1,'Do you like this forum?','So, what do you say? Do you like the result of this project?','2011-04-04 18:35:24'),
-	(3,1,2,'How can I write a thread???','Can anybody tell me how I can write a new thread????????????????\n\nTell me immediately!','2011-04-05 23:08:10');
+	(3,1,2,'How can I write a thread???','Can anybody tell me how I can write a new thread?','2011-04-05 23:08:10'),
+	(4,1,1,'TEST','testsdadasd','2011-04-15 21:43:00'),
+	(5,1,1,'wwwawwa','wwww','2011-04-15 22:13:35'),
+	(6,1,1,'sadsadsad','sadasdsadsa','2011-04-16 12:54:54'),
+	(7,2,1,'XHTMLAsiajsd','jakhsdkajshdsadasdsadsa','2011-04-16 12:55:09'),
+	(8,1,1,'ewfewfew','fewfewfewf','2011-04-19 10:42:47');
 
 /*!40000 ALTER TABLE `thread` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -137,21 +148,27 @@ CREATE TABLE `user` (
   `username` varchar(16) NOT NULL,
   `password` varchar(32) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(50) DEFAULT NULL,
+  `dateOfBirth` datetime DEFAULT NULL,
+  `permission` int(10) unsigned DEFAULT '0',
   `active` tinyint(1) NOT NULL DEFAULT '1',
-  `permission` int(5) NOT NULL DEFAULT '0',
+  `createdOn` datetime NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`userId`,`username`,`password`,`email`,`active`,`permission`)
+INSERT INTO `user` (`userId`,`username`,`password`,`email`,`website`,`location`,`firstName`,`lastName`,`dateOfBirth`,`permission`,`active`,`createdOn`)
 VALUES
-	(1,'davidknezic','c5b01c8f8475578020b83e0c3292adf9','davidknezic@gmail.com',1,0),
-	(2,'marcello','de239033eaac1ffb65f83ab6e4dd0981','marcel@voelki.ch',1,0),
-	(3,'sven','3346a536dd4a2c2340ea25c92a40c0d4','github@eniu.ch',1,2),
-	(4,'blocked','61326117ed4a9ddf3f754e71e119e5b3','block@me.now',0,0),
-	(5,'master','eb0a191797624dd3a48fa681d3061212','master@master.master',1,0);
+	(1,'david','172522ec1028ab781d9dfd17eaca4427','davidknezic@gmail.com','www.davidknezic.com','Winterthur (ZH), Swintzerland','David','Knezic','1994-01-28 00:00:00',0,1,'2011-00-00 00:00:00'),
+	(2,'marcello','de239033eaac1ffb65f83ab6e4dd0981','marcel@bloeckli.ch',NULL,NULL,NULL,NULL,NULL,0,1,'0000-00-00 00:00:00'),
+	(3,'eniu','3346a536dd4a2c2340ea25c92a40c0d4','forum@eniu.ch',NULL,NULL,NULL,NULL,NULL,0,1,'0000-00-00 00:00:00'),
+	(4,'blocked','61326117ed4a9ddf3f754e71e119e5b3','block@me.now',NULL,NULL,NULL,NULL,NULL,0,0,'0000-00-00 00:00:00'),
+	(5,'master','eb0a191797624dd3a48fa681d3061212','master@master.xyz',NULL,NULL,NULL,NULL,NULL,0,1,'0000-00-00 00:00:00');
 
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
