4b805efe9b4c6f5b93bb7ed7e70a2b34a09cb6ac
Sven Tschui <info@eniu.ch>
2011-04-24 12:28:02 +0200

    Added Caching to the BoardBean
    BoardBean is now RequestScoped and Requests are cached

--- src/beans/BoardBean.java
+++ src/beans/BoardBean.java
@@ -4,25 +4,50 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import models.BoardModel;
 import models.ThreadModel;
-
+/**
+ * 
+ * @author Sven Tschui
+ *
+ */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class BoardBean {
+	private BoardModel board;
+	private ArrayList<ThreadModel> threads;
+
+	/**
+	 * Get board which is requesetd via RequestParameterMap
+	 * @return BoardModel board requested via RequestParameterMap
+	 * @throws Throwable
+	 */
 	public BoardModel getBoard() throws Throwable {
-		Map<String, String> request = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-		Integer boardId = Integer.parseInt(request.get("boardId"));
-		
-		if(boardId == null) throw new Exception("Invalid Request");
-		
-		return new BoardModel(boardId);
+		if (board == null) {
+			Map<String, String> request = FacesContext.getCurrentInstance()
+					.getExternalContext().getRequestParameterMap();
+			Integer boardId = Integer.parseInt(request.get("boardId"));
+
+			if (boardId == null)
+				throw new Exception("Invalid Request");
+
+			board = new BoardModel(boardId);
+		}
+
+		return board;
 	}
-	
+
+	/**
+	 * Get threads to the requested Board
+	 * @return ArrayList<ThreadModel> Requested Threads
+	 * @throws Throwable
+	 */
 	public ArrayList<ThreadModel> getThreads() throws Throwable {
-		return getBoard().getThreads(0, 20);
+		if (threads == null)
+			threads = getBoard().getThreads(0, 20);
+		return threads;
 	}
 }
