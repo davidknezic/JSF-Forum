f81d9aa63a015d8f4d2adbdd75b9725c7c2676a9
Sven Tschui <info@eniu.ch>
2011-06-07 14:12:41 +0200

    Added Accesschecker. Now we have to rework all facelets and remove self access checking.

---
+++ src/beans/AccessChecker.java
@@ -0,0 +1,64 @@
+package beans;
+
+import java.util.HashMap;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+import models.UserModel;
+
+public class AccessChecker implements PhaseListener {
+	private HashMap<String, Integer> permissions = new HashMap<String, Integer>();
+
+	public AccessChecker() {
+		permissions.put("/editProfile.xhtml", UserModel.USER);
+		permissions.put("/newBoard.xhtml", UserModel.USER);
+		permissions.put("/newThread.xhtml", UserModel.USER);
+	}
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+
+		FacesContext fc = event.getFacesContext();
+
+		// Check to see if they are on the login page.
+		String viewId = fc.getViewRoot().getViewId();
+
+		System.out.println(viewId);
+
+		Integer permission = permissions.get(viewId);
+
+		if (permission != null && !hasPermission(permission)) {
+			fc.getApplication()
+					.getNavigationHandler()
+					.handleNavigation(fc, null, "/insufficientPermission.xhtml");
+			System.out.println("...");
+			return;
+		}
+
+		System.out.println("ok");
+	}
+
+	private boolean hasPermission(int perm) {
+		LoginBean loginBean = (LoginBean) FacesContext.getCurrentInstance()
+				.getExternalContext().getSessionMap().get("loginBean");
+
+		if (loginBean == null || loginBean.getLoggedin() == false
+				|| perm > loginBean.getUser().getPermission())
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+
+}
