b02990961eb1039903c14391dad296c7831ae2cf
Sven Tschui <info@eniu.ch>
2011-06-09 22:28:39 +0200

    Current navigation tab is now visualy highlited

---
+++ src/beans/NavigationHelper.java
@@ -0,0 +1,20 @@
+package beans;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+@ManagedBean
+@ApplicationScoped
+public class NavigationHelper {
+	private static final String ACTIVE_CLASS = "active";
+	
+	public String getClass(String facelet) {
+		String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
+		System.out.println(viewId);
+		if(viewId.equals(facelet))
+			return NavigationHelper.ACTIVE_CLASS;
+		return "";
+	}
+}
